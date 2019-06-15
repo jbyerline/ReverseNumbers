d0e866867026e58fca8c7908eb57a28e6b61db92
Jacob Byerline <47374239+jbyerline@users.noreply.github.com>
2019-06-14 17:18:03 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ReverseNumbers
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // Print instructions to console
+            Console.WriteLine("Reverse Numbers");
+            Console.WriteLine("Enter Number: ");
+
+            // Read String input from console
+            string sInput = Console.ReadLine();
+
+            // Convert String to an array of Int
+            var aIntArray = sInput.Select(c => c - '0').ToArray();
+
+            // Convert Int Array to Int List
+            List<int> lIntList = aIntArray.OfType<int>().ToList();
+
+            // Reverse List
+            lIntList.Reverse();
+
+            // Convert reversed list back to string
+            var sAnswer = string.Join("", lIntList);
+
+            // Print String answer to Console
+            Console.WriteLine("The reverse of: " + sInput + " is: " + sAnswer);
+
+        }
+    }
+}
